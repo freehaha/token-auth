183d68dc172385d7397a9bb95d108295338ce09c
Freehaha <freehaha@gmail.com>
2014-07-16 13:42:38 +1000

    update examples

--- examples/handlefunc/main.go
+++ examples/handlefunc/main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/freehaha/token-auth"
+	"github.com/freehaha/token-auth/memory"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	memStore := tauth.NewMemoryTokenStore("salty")
+	memStore := memstore.New("salty")
 	tokenAuth := tauth.NewTokenAuth(nil, nil, memStore)
 
 	mux.HandleFunc("/login", func(w http.ResponseWriter, req *http.Request) {
 		t := memStore.NewToken("User1")
-		fmt.Fprintf(w, "hi User1, your token is %s", t.Token)
+		fmt.Fprintf(w, "hi User1, your token is %s", t)
 	})
 
 	mux.HandleFunc("/restricted", tokenAuth.HandleFunc(func(w http.ResponseWriter, req *http.Request) {
 		token := tauth.Get(req)
-		fmt.Fprintf(w, "hi %s", token.Id)
+		fmt.Fprintf(w, "hi %s", token.Claims("id").(string))
 	}))
 
 	fmt.Println("listening at :3000")
--- examples/subroute/main.go
+++ examples/subroute/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/freehaha/token-auth"
+	"github.com/freehaha/token-auth/memory"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	memStore := tauth.NewMemoryTokenStore("salty")
+	memStore := memstore.New("salty")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/login/{id}", func(w http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 		t := memStore.NewToken(vars["id"])
-		fmt.Fprintf(w, "hi %s, your token is %s", vars["id"], t.Token)
+		fmt.Fprintf(w, "hi %s, your token is %s", vars["id"], t)
 	})
 
 	rRestrict := mux.NewRouter()
 	s := rRestrict.PathPrefix("/restricted/").Subrouter()
 	s.HandleFunc("/area", func(w http.ResponseWriter, req *http.Request) {
 		token := tauth.Get(req)
-		fmt.Fprintf(w, "hi %s", token.Id)
+		fmt.Fprintf(w, "hi %s", token.Claims("id"))
 	})
 	tokenAuth := tauth.NewTokenAuth(rRestrict, nil, memStore)
 	r.PathPrefix("/restricted").Handler(tokenAuth)
